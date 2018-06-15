170ce9cd78669a97ed9eca930e7422d3eea62e2e
Terry Chu <terrychu@qnap.com>
2018-06-15 09:24:37 +0800

    fix: move joomla web into the volume from baseImage (/var/www/html)

--- Dockerfile
+++ Dockerfile
@@ -5,7 +5,7 @@ ENV \
   JOOMLA_VERSION=3.8.1 \
   JOOMLA_SHA1=b31e97ef16e6030156600b4b9e8073eaf3b28928 \
   JOOMLA_INSTALLATION_DISABLE_LOCALHOST_CHECK=1 \
-  WEBROOT=/var/joomla \
+  WEB_ROOT=/var/joomla \
   DB_NAME=joomla \
   DB_USER=joomla \
   DB_PASSWORD=joomla \
@@ -13,15 +13,13 @@ ENV \
 
 RUN curl -o joomla.zip -SL https://github.com/joomla/joomla-cms/releases/download/${JOOMLA_VERSION}/Joomla_${JOOMLA_VERSION}-Stable-Full_Package.zip \
   && echo "$JOOMLA_SHA1 *joomla.zip" | sha1sum -c - \
-  && rm /var/www/html/index.php \
-  && mkdir $WEBROOT \
-  && unzip joomla.zip -d $WEBROOT \
-  && chown -R nginx:nginx $WEBROOT \
+  && mkdir $WEB_ROOT \
+  && unzip joomla.zip -d $WEB_ROOT \
+  && chown -R nginx:nginx $WEB_ROOT \
   && rm joomla.zip
 
-RUN sed -r 's|^(Options -Indexes.*)$|#\1|' /var/joomla/htaccess.txt > ${WEBROOT}/.htaccess
+RUN sed -r 's|^(Options -Indexes.*)$|#\1|' ${WEB_ROOT}/htaccess.txt > ${WEB_ROOT}/.htaccess
 
 ADD entrypoint.sh /entrypoint.sh
 
-VOLUME ["/usr/joomla"]
 CMD ["/entrypoint.sh"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
-sed -i 's|default="localhost"|default="'"$DB_HOST"'"|' $WEBROOT/installation/model/forms/database.xml
-sed -i 's|label="INSTL_DATABASE_USER_LABEL"$|label="INSTL_DATABASE_USER_LABEL" default="'"$DB_USER"'"|' $WEBROOT/installation/model/forms/database.xml
-sed -i 's|label="INSTL_DATABASE_PASSWORD_LABEL"$|label="INSTL_DATABASE_PASSWORD_LABEL" default="'"$DB_PASSWORD"'"|' $WEBROOT/installation/model/forms/database.xml
-sed -i 's|label="INSTL_DATABASE_NAME_LABEL"$|label="INSTL_DATABASE_NAME_LABEL" default="'"$DB_NAME"'"|' $WEBROOT/installation/model/forms/database.xml
+if ! grep -iq joomla /var/www/html/index.php; then
+    echo "Joomla not found in /var/www/html - copying now..."
+    rm -f /var/www/html/index.php
+    sed -i 's|default="localhost"|default="'"$DB_HOST"'"|' $WEB_ROOT/installation/model/forms/database.xml
+    sed -i 's|label="INSTL_DATABASE_USER_LABEL"$|label="INSTL_DATABASE_USER_LABEL" default="'"$DB_USER"'"|' $WEB_ROOT/installation/model/forms/database.xml
+    sed -i 's|label="INSTL_DATABASE_PASSWORD_LABEL"$|label="INSTL_DATABASE_PASSWORD_LABEL" default="'"$DB_PASSWORD"'"|' $WEB_ROOT/installation/model/forms/database.xml
+    sed -i 's|label="INSTL_DATABASE_NAME_LABEL"$|label="INSTL_DATABASE_NAME_LABEL" default="'"$DB_NAME"'"|' $WEB_ROOT/installation/model/forms/database.xml
+    cp -r $WEB_ROOT/* /var/www/html/ # no rsync in the base image
+    echo "Complete! Joomla has been successfully copied to /var/www/html"
+fi
 
 # setting ipv6 config
 if [ x"$IPV6_LISTEN" == x"false" ]; then
